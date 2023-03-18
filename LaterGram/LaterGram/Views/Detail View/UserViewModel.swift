e5ddf1f602caacce5b93ac78b6d40a83c93642c1
Colton Brenneman <colton.brenneman@gmail.com>
2023-03-18 06:24:36 -0600

    turned in

---
+++ LaterGram/LaterGram/Views/Detail View/UserViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  UserViewModel.swift
+//  LaterGram
+//
+//  Created by Colton Brenneman on 3/17/23.
+//
+
+import Foundation
+import FirebaseFirestore
+
+struct UserViewModel {
+    
+    //MARK: - Properties
+    var user: User?
+    
+}
